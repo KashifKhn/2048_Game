529430f7fdba629678f56b717b95cc93c3670d64
KashifKhn <kashifkhnx04@gmail.com>
2024-11-03 09:23:50 +0500

    feat: add constatns

---
+++ src/constants/Game.ts
@@ -0,0 +1,5 @@
+import { BoardSizeType, DirectionType } from "@/types/Game";
+
+export const BoardSize: BoardSizeType = [3, 4, 5, 6, 8];
+
+export const Directions: DirectionType[] = ["up", "down", "left", "right"];
